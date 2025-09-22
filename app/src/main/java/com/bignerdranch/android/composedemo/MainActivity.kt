04b9cdaefdd98bf99760a518e64e937b44954786
AAM1337 <amacinov@gmail.com>
2025-09-22 19:15:41 +0700

    DemoSlider

--- app/src/main/java/com/bignerdranch/android/composedemo/MainActivity.kt
+++ app/src/main/java/com/bignerdranch/android/composedemo/MainActivity.kt
@@ -7,11 +7,13 @@ import androidx.activity.enableEdgeToEdge
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.padding
 import androidx.compose.material3.Scaffold
+import androidx.compose.material3.Slider
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.text.font.FontWeight
 import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
 import androidx.compose.ui.unit.sp
 import com.bignerdranch.android.composedemo.ui.theme.ComposeDemoTheme
 
@@ -35,8 +37,15 @@ fun DemoText(message: String, fontSize: Float){
         fontWeight = FontWeight.Bold
     )
 }
-
-
+@Composable
+fun DemoSlider(sliderPosition: Float, onPositionChange: (Float) -> Unit){
+    Slider(
+        modifier = Modifier.padding(10.dp),
+        valueRange = 20f..38f,
+        value = sliderPosition,
+        onValueChange = onPositionChange
+    )
+}
 @Preview
 @Composable
 fun DemoTextPreview()
